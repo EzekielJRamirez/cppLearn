2679491d90436189b2a276a67909a80e0e3d3465
Ezekiel Ramirez <ezekiel.ramirez@calbaptist.edu>
2023-03-14 19:24:21 -0700

    first push

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * Ezekiel Ramirez
+ * https://youtu.be/8jLOx1hD3_o?t=6191
+*/
+#include <iostream>
+
+////////////////////////////
+// single line comment
+
+int main() {
+    auto result = (10 <=> 20) > 0;
+    std::cout << result << std::endl;
+}
